9714e79346279735fe1676d65414871dfee3dc3c
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-01-04 13:47:12 +0100

    build: constructor dni

--- dni.py
+++ dni.py
@@ -1,1 +1,10 @@
-dni
+import tabla_asignacion
+
+class Dni:
+
+    # Constructor DNI, recogiendo la tabla de asignación de la clase TablaAsignacion.
+    def __init__(self, cadena = ""):
+        self.dni = cadena
+        self.tabla = tabla_asignacion.TablaAsignacion()
+
+    
