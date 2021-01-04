1391f01e85f8765de7579aa2f7cbf924d206c0e8
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-01-04 13:43:31 +0100

    build: tablaletras constructor

---
+++ tabla_asignacion.py
@@ -0,0 +1,7 @@
+class TablaAsignacion:
+
+    # Constructor de la tabla de asignación de letras validas para un DNI.
+    def __init__(self):
+        self.tablaLetras = [ 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' ]
+
+    
