15f1b218a07c5158d390f67390bebfcf50c048aa
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-01-08 13:37:16 +0100

    feat: buildDni method

--- src/dni.py
+++ src/dni.py
@@ -1,4 +1,4 @@
-from tabla_asignacion import *
+from src.tabla_asignacion import *
 
 class Dni:
 
@@ -7,4 +7,11 @@ class Dni:
         self.dni = cadena
         self.tabla = TablaAsignacion()
 
-    
+    # Metodo encargado de construir un numero de DNI valido a partir de un numero que cumpla con las condiciones de longitud establecidas en las constantes de la clase TablaAsignacion.
+    def buildDni(number):
+        if len(number) == TablaAsignacion.DNI_NUMBER_PART_LENGHT:
+            DNI = str(number)+ TablaAsignacion.getLetra(number)
+            if len(DNI) == TablaAsignacion.DNI_TOTAL_LENGTH:
+                return DNI
+        else:
+            print("El numero de DNI es incorrecto")
