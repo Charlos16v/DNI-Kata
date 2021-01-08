c29f428c693063e5ab80cbbb9234e1b56493530a
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-01-08 13:37:26 +0100

    feat: configuration constants

--- src/tabla_asignacion.py
+++ src/tabla_asignacion.py
@@ -5,6 +5,9 @@ class TablaAsignacion:
         self.tablaLetras = [ 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' ]
 
 
+    DNI_TOTAL_LENGTH = 9
+    DNI_NUMBER_PART_LENGHT = DNI_TOTAL_LENGTH-1
+
     # Getter encargado de obtener la letra de la tabla de asignación a partir de su posición, se encuentra dentro de una estructura try/catch para poder alcanzar posibles excepciones, principalmente la excepcion de que la letra se encuentre fuera del rango de alcance en la lista de letras.
     def getLetra(self, pos):
         try:
